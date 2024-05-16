76374bd5aefd7fa16938f0dfcb4171544c1f4fe9
Josh Caponigro <joshcap@email.unc.edu>
2024-05-16 03:17:26 -0400

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,89 @@ extern crate url;
 
 use clap::{Arg, App};
 use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::blocking::get;
+use reqwest::blocking::Client;
 use scraper::{Html, Selector};
 use std::collections::{HashSet, VecDeque};
 use url::Url;
+use std::error::Error;
+
+struct Crawler {
+    client: Client,
+    visited: HashSet<Url>,
+    queue: VecDeque<Url>,
+    base_host: String,
+    progress_bar: ProgressBar,
+}
+
+impl Crawler {
+    fn new(base_url: Url) -> Result<Self, Box<dyn Error>> {
+        let client = Client::builder().build()?;
+        let progress_bar = create_progress_bar(false, "Crawling...".to_string(), None);
+        Ok(Crawler {
+            client,
+            visited: HashSet::new(),
+            queue: VecDeque::from([base_url.clone()]),
+            base_host: base_url.host_str().ok_or("Invalid host")?.to_string(),
+            progress_bar,
+        })
+    }
+
+    fn crawl(&mut self) {
+        while let Some(url) = self.queue.pop_front() {
+            if self.visited.contains(&url) {
+                continue;
+            }
+
+            self.progress_bar.set_message(format!("Crawling: {}", url));
+            if let Ok(links) = self.fetch_and_parse(&url) {
+                for link in links {
+                    if !self.visited.contains(&link) && link.host_str() == Some(&self.base_host) {
+                        self.queue.push_back(link);
+                    }
+                }
+            }
+
+            self.visited.insert(url);
+        }
+        self.progress_bar.finish_with_message("Crawling complete");
+        println!("Visited pages:");
+        for url in &self.visited {
+            println!("{}", url);
+        }
+    }
+
+    fn fetch_and_parse(&self, url: &Url) -> Result<HashSet<Url>, Box<dyn Error>> {
+        let body = self.client.get(url.as_str()).send()?.text()?;
+        let document = Html::parse_document(&body);
+        let selector = Selector::parse("a").unwrap();
+        let mut links = HashSet::new();
+
+        for element in document.select(&selector) {
+            if let Some(link) = element.value().attr("href") {
+                if let Ok(link_url) = url.join(link) {
+                    links.insert(link_url);
+                }
+            }
+        }
+
+        Ok(links)
+    }
+}
+
+fn create_progress_bar(quiet_mode: bool, msg: String, length: Option<u64>) -> ProgressBar {
+    let bar = if quiet_mode {
+        ProgressBar::hidden()
+    } else {
+        let mut builder = ProgressBar::new_spinner();
+        if let Some(len) = length {
+            builder = ProgressBar::new(len);
+        }
+        builder.set_message(msg.clone());
+        builder.set_style(ProgressStyle::default_spinner());
+        builder
+    };
+    bar
+}
 
 fn main() {
     let matches = App::new("rust-crawler")
@@ -22,83 +101,11 @@ fn main() {
             .index(1)
             .help("URL to start crawling"))
         .get_matches();
-        
+
     let start_url = matches.value_of("URL").unwrap();
     let parsed_url = Url::parse(start_url).expect("Invalid URL");
 
-    let progress_bar = create_progress_bar(false, "Crawling...".to_string(), None);
-
-    let mut visited = HashSet::new();
-    let mut queue = VecDeque::new();
-    queue.push_back(parsed_url.clone());
-
-    while let Some(url) = queue.pop_front() {
-        if visited.contains(&url) {
-            continue;
-        }
-
-        progress_bar.set_message(format!("Crawling: {}", url));
-        match fetch_and_parse(&url) {
-            Ok(links) => {
-                for link in links {
-                    if !visited.contains(&link) && link.host() == parsed_url.host() {
-                        queue.push_back(link);
-                    }
-                }
-            }
-            Err(e) => {
-                eprintln!("Failed to fetch {}: {}", url, e);
-            }
-        }
-
-        visited.insert(url);
+    if let Ok(mut crawler) = Crawler::new(parsed_url) {
+        crawler.crawl();
     }
-
-    progress_bar.finish_with_message("Crawling complete");
-    println!("Visited pages:");
-    for url in visited {
-        println!("{}", url);
-    }
-}
-
-fn create_progress_bar(quiet_mode: bool, msg: String, length: Option<u64>) -> ProgressBar {
-    let bar = if quiet_mode {
-        ProgressBar::hidden()
-    } else {
-        match length {
-            Some(len) => ProgressBar::new(len),
-            None => ProgressBar::new_spinner(),
-        }
-    };
-
-    bar.set_message(msg.clone());
-    if let Some(_) = length {
-        bar.set_style(
-            ProgressStyle::default_bar()
-                .template("{msg} {spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
-                .expect("Failed to set progress style")
-                .progress_chars("=> ")
-        );
-    } else {
-        bar.set_style(ProgressStyle::default_spinner());
-    }
-
-    bar
-}
-
-fn fetch_and_parse(url: &Url) -> Result<HashSet<Url>, Box<dyn std::error::Error>> {
-    let body = get(url.as_str())?.text()?;
-    let document = Html::parse_document(&body);
-    let selector = Selector::parse("a").unwrap();
-    let mut links = HashSet::new();
-
-    for element in document.select(&selector) {
-        if let Some(link) = element.value().attr("href") {
-            if let Ok(link_url) = url.join(link) {
-                links.insert(link_url);
-            }
-        }
-    }
-
-    Ok(links)
 }
